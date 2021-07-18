f01e7663dbd06239ea3ba7e150caedb77d196765
triglace57 <triglace57@users.noreply.github.com>
2021-07-18 13:43:06 +0200

    Version 1.0.0
    Reponsive OK

---
+++ app/main.js
@@ -0,0 +1,31 @@
+const faders_left = document.querySelectorAll(".fade_in")
+const faders_right = document.querySelectorAll(".fade_in_right")
+const appearOptions = {
+    threshold: 0.3
+}
+
+const appearWithScroll = new IntersectionObserver((entries,appearWithScroll)=>{
+    entries.forEach(entry => {
+        if(!entry.isIntersecting){return;}
+        entry.target.classList.add("appear");
+        appearWithScroll.unobserve(entry.target)
+        return;
+    });
+},appearOptions)
+const appearWithScrollRight = new IntersectionObserver((entries,appearWithScroll)=>{
+    entries.forEach(entry => {
+        if(!entry.isIntersecting){return;}
+        entry.target.classList.add("appear_right");
+        appearWithScroll.unobserve(entry.target)
+        return;
+    });
+},appearOptions)
+
+faders_left.forEach(fader_left => {
+    console.log(fader_left)
+    appearWithScroll.observe(fader_left)
+});
+faders_right.forEach(fader_right => {
+    console.log(fader_right)
+    appearWithScrollRight.observe(fader_right)
+})
